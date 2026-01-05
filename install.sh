eebbb5d50daf4cc220378b8797377cbe1eba3be3
zodiakdrinker <mateoaja11@gmail.com>
2026-01-05 22:26:54 +0100

    Automatic installation Arch

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#### install.sh
+# Check UID
+[ "$(id -u)" -eq 0 ] && echo "error: avoid running autoArch.sh as root/sudo." && exit
+
+
+# install packages
+
+echo -ne "\n[+] Installing required packages\n"
+sudo pacman -S --noconfirm git lsd bat kitty lightdm lightdm-slick-greeter bspwm wmname sxhkd polybar zsh feh picom rofi nano unzip zsh noto-fonts-emoji zsh-syntax-highlighting zsh-autosuggestions wget firefox flameshot neovim gdb gcc openvpn xclip
+
+# create directories
+echo -ne "\n[+] Setting up directories\n"
+mkdir $HOME/.config
+cp -r configurations/* $HOME/.config/
+
+find $HOME/.config/polybar/ -type f -exec chmod +x {} \; 
+
+mkdir $HOME/.local/share/fonts
+cp -r fonts/* $HOME/.local/share/fonts
+fc-cache -fv
+
+sudo chmod +x ~/.config/sxhkd/sxhkdrc
+sudo chmod +x ~/.config/bspwm/bspwmrc
+sudo chmod +x ~/.config/polybar/launch.sh
+sudo chmod +x ~/.config/bspwm/scripts/bspwm_resize
+
+echo -ne "\n[+] Creating bspwm.desktop for LightDM\n"
+sudo mkdir -p /usr/share/xsessions
+sudo mkdir -p /usr/share/backgrounds
+sudo cp configurations/Wallpapers/greeter-wall.jpg /usr/share/backgrounds/
+
+cat <<EOF | sudo tee /usr/share/xsessions/bspwm.desktop > /dev/null
+[Desktop Entry]
+Name=bspwm
+Comment=Tiling window manager
+Exec=bspwm
+TryExec=bspwm
+Type=Application
+DesktopNames=bspwm
+EOF
+
+sudo sed -i '/\[Seat:\*\]/a greeter-session=lightdm-slick-greeter' /etc/lightdm/lightdm.conf
+
+cat <<EOF | sudo tee /etc/lightdm/slick-greeter.conf > /dev/null
+[Greeter]
+show-hostname=false
+background=/usr/share/backgrounds/greeter-wall.jpg
+font-name=HackNerdFont
+EOF
+
+echo -ne "\n[✔] Minimal bspwm setup done. Select it from LightDM at login.\n"
+
+echo -ne "\n[+] Installing NvChad"
+
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim --headless +qa
+rm -rf ~/.config/nvim/.git
+
+echo -ne "\n[✔] NvChad installed on nvim" 
+
+sh postinstall.sh
---
+++ postinstall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo -ne "\n[+] Fixing .zshrc\n"
+
+cp configurations/zshrc_template ~/.zshrc
+
+echo -ne "\n[+] Instaling starship terminal"
+
+curl -sS https://starship.rs/install.sh | sh
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+cp configurations/starship_template ~/.config/starship.toml
+
+echo -ne "\n[✔] Starship installed" 
+
+# make symlink in root
+sudo ln -sf $HOME/.zshrc /root/.zshrc
+sudo chsh -s /usr/bin/zsh root 
+
+## blackarch
+read -p "Do you want to install the BlackArch repositories?[Y/n]: " choice
+choice=${choice:-Y}
+if [[ "$choice" == "Y" || "$choice" == "y" ]]; then 
+  curl -O https://blackarch.org/strap.sh
+  sudo sh strap.sh
+  echo -ne "\n[✔] BlackArch Repositories installed"
+elif [[ "$choice" == "N" || "$choice" == "n" ]]; then
+  echo -ne "\n[✖]The Respositories has not been installed"
+else
+  echo -ne "\n[✖]The Respositories has not been installed"
+fi 
+
+# clean
+echo -ne "\n[+] Cleaning up...\n "
+sudo pacman -Sy archlinux-keyring
+
+echo -ne "\n[+] DONE!\n"
